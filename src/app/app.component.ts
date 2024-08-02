f20121df253e5e6e8feedc2846315851dff9b5e4
Leonardo Aquino <laquino@soaint.com>
2024-08-02 17:00:28 -0500

    Feat: Uso de las funcionalidades en app-component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StandAloneExampleComponent } from './stand-alone-example/stand-alone-example.component';
+import { ProfilePhotoComponent } from './selectors/profile-photo/profile-photo.component';
+import { AttributeSelectorComponent } from './selectors/attribute-selector/attribute-selector.component';
+import { AttributeSelectorTooltipComponent } from './selectors/attribute-selector-tooltip/attribute-selector-tooltip.component';
+import { ClassSelectorComponent } from './selectors/class-selector/class-selector.component';
+import { LifeCycleComponent } from './life-cycle/life-cycle.component';
+import { CommonModule } from '@angular/common';
+import { CustomBaseBasicComponent } from './inheritance/basic/custom-base-basic/custom-base-basic.component';
+import { AlumnoComponent } from './inheritance/school/alumno/alumno.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    StandAloneExampleComponent, ProfilePhotoComponent,
+    AttributeSelectorComponent, AttributeSelectorTooltipComponent,
+    ClassSelectorComponent,
+    LifeCycleComponent,
+    CustomBaseBasicComponent,
+    AlumnoComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'componentes-proyect';
+  title = 'Componentes en Angular 17+';
+  showChild = true;
+  toggleChild() {
+    this.showChild = !this.showChild;
+  }
 }
