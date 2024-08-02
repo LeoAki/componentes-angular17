2c13054222f287dea8f9decd10d4c6378e2696e1
Leonardo Aquino <laquino@soaint.com>
2024-08-02 16:59:33 -0500

    Feat: Ruteo para el componente sin selector.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { UserProfileComponent } from './selectors/user-profile/user-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'sin-selector', component: UserProfileComponent}
+];
